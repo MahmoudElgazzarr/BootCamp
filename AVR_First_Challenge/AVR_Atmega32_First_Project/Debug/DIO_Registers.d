ed312a9cf0136c41e7edb67ae5b9b8b2a7fad59c
Mahmoud Elgazzar <mahmoud.elgazzar@avelabs.com>
2019-02-17 18:08:42 +0200

    Test

---
+++ AVR_First_Challenge/AVR_Atmega32_First_Project/Debug/DIO_Registers.d
@@ -0,0 +1,11 @@
+DIO_Registers.d DIO_Registers.o: .././DIO_Registers.c .././types.h \
+ .././DIO_Registers.h .././DIO_Registers_Definitions.h \
+ .././BitwiseOperation.h
+
+.././types.h:
+
+.././DIO_Registers.h:
+
+.././DIO_Registers_Definitions.h:
+
+.././BitwiseOperation.h:
---
+++ AVR_First_Challenge/AVR_Atmega32_First_Project/Debug/main.d
@@ -0,0 +1,8 @@
+main.d main.o: .././main.c .././Types.h .././DIO_Registers_Definitions.h \
+ .././DIO_Registers.h
+
+.././Types.h:
+
+.././DIO_Registers_Definitions.h:
+
+.././DIO_Registers.h:
